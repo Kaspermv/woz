f73b319bf6d8c49679c452871e1ec06a3eb575f5
Jonas Beltoft <jonasb5@hotmail.dk>
2020-11-17 10:28:25 +0100

    Initial JavaFX code

--- worldofzuul/worldofzuul/Start.java
+++ worldofzuul/worldofzuul/Start.java
@@ -1,8 +1,25 @@
 package worldofzuul;
 
-public class Start {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent window = new AnchorPane();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(window, 300, 275));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
         Game start = new Game();
         start.play();
+
     }
 }
