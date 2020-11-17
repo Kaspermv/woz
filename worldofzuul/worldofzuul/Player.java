91a0a0c8bc527532d81eda2bafcc39a38b7ebeb7
Hannehue <hannehue@gmail.com>
2020-11-17 10:42:23 +0100

    Added Player Class

---
+++ worldofzuul/worldofzuul/Player.java
@@ -0,0 +1,45 @@
+package worldofzuul;
+
+public class Player {
+    private boolean canSleep = false;
+    public int balance = 1000;
+    public int income = 0;
+    public Inventory inventory;
+    public int lifeQuality = 100;
+
+
+    public Player(){
+
+    }
+
+    public boolean isCanSleep() {
+        return canSleep;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public int getIncome() {
+        return income;
+    }
+
+    public int getLifeQuality() {
+        return lifeQuality;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setCanSleep(boolean canSleep) {
+        this.canSleep = canSleep;
+    }
+
+    public void setLifeQuality(int lifeQuality) {
+        this.lifeQuality = lifeQuality;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+}
