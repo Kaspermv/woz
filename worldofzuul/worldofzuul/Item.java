5ba68aa7f4bdbd8c7c674acf8706799c838e8978
Hannehue <Hannehue@gmail.com>
2020-12-02 16:36:09 +0100

    Converted inventory Arraylist to Hashmap

--- worldofzuul/worldofzuul/Item.java
+++ worldofzuul/worldofzuul/Item.java
@@ -4,11 +4,13 @@ public class Item {
     private String name;
     private String description;
     private int price;
+    private int id;
 
-    public Item(String name, String description, int price) {
+    public Item(String name, String description, int price, int id) {
         this.name = name;
         this.description = description;
         this.price = price;
+        this.id = id;
     }
 
     @Override
@@ -22,5 +24,21 @@ public class Item {
 
     public int getPrice() {
         return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            Integer i = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
