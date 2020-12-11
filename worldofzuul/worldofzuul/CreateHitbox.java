b966e4dd4560f23d22e167c07c2c7776f35024ba
Jonas Beltoft <jonasb5@hotmail.dk>
2020-12-11 15:27:23 +0100

    Final comment

--- worldofzuul/worldofzuul/CreateHitbox.java
+++ worldofzuul/worldofzuul/CreateHitbox.java
@@ -6,6 +6,7 @@ import javafx.scene.shape.Rectangle;
 
 public final class CreateHitbox {
 
+    // Static method for creating a rectangle, used as hitboxes, from an ImageView object
     public static Rectangle imageView(ImageView sourceImgView){
         Rectangle hitboxRect = new Rectangle(sourceImgView.getLayoutX(),sourceImgView.getLayoutY(), sourceImgView.getImage().getWidth(),sourceImgView.getImage().getHeight());
         return hitboxRect;
--- worldofzuul/worldofzuul/Start.java
+++ worldofzuul/worldofzuul/Start.java
@@ -11,15 +11,20 @@ public class Start extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // Instantiates the game view
         ViewManager manager = new ViewManager();
+        // Creates the key listeners for user input
         manager.createKeyListeners();
+        // Creates the main game loop
         manager.createGameLoop();
+        // Sets the primary stage of the application to the viewManager stage
         primaryStage = manager.getMainStage();
+        // Sets the window title
         primaryStage.setTitle("World of Slum");
         // Final update of canvas
         primaryStage.show();
     }
-//Creates a new instance of Game
+// Launches the game on startup
     public static void main(String[] args) {
         launch(args);
     }
